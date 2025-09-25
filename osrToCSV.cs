3372fbe9767e3f6feb8b37947e2c7efcbb3c9fd5
Nikku <109353025+Nikku514@users.noreply.github.com>
2025-09-24 23:30:47 -0400

    Add ability to use multiple files and some cleanup

--- osrToCSV.cs
+++ osrToCSV.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Diagnostics;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,48 +8,51 @@ using ReplayAPI;
 
 namespace osrToCSV
 {
-    public class osrToCSV
+    public class OSRtoCSV
     {
         static void Main(string[] args)
         {
-            string ReplayPath = null;
+            string replayPath;
             if (File.Exists("Options.cfg"))
             {
                 StreamReader sr = new StreamReader("Options.cfg");
-                ReplayPath = sr.ReadLine();
+                replayPath = sr.ReadLine();
                 sr.Close();
-            }
-            else //NTODO: unfuck this so that Replay.cs doesnt complain if it's not perfectly a valid .osr
-            {
-                Console.WriteLine("Enter replay path:");
-                ReplayPath = Console.ReadLine();
-                StreamWriter RP = new StreamWriter("Options.cfg");
-                RP.WriteLine(ReplayPath);
-                RP.Close();
-            }
-
-            Replay ry = new Replay(ReplayPath); //NTODO: somehow get this shitter to allow multiple files per run of the exe
-
-            Console.WriteLine("Writing "+ReplayPath);
-            if (File.Exists("Replays.csv"))
-            {
-                using (StreamWriter sw = File.AppendText("Replays.csv"))
-                { 
-                sw.WriteLine(ry.PlayerName + "," + ry.GameMode + "," + ry.Version + "," + ry.Mods + "," + ry.TotalScore + "," + ry.Count300 + "," + ry.Count100 + "," + ry.Count50 + "," + ry.CountGeki + "," + ry.CountKatu + "," + ry.CountMiss + "," + ry.MaxCombo + "," + ry.IsPerfect + "," + ry.MapHash);
-                sw.Close();
-                }
             }
             else
             {
+                Console.WriteLine("Enter replay path:");
+                replayPath = Console.ReadLine();
+                StreamWriter RP = new StreamWriter("Options.cfg");
+                RP.WriteLine(replayPath);
+                RP.Close();
+            }
+            if (!File.Exists("Replays.csv"))
+            {
                 StreamWriter sw = new StreamWriter("Replays.csv");
-                sw.WriteLine("Name,Mode,Version,Mods,Score,300s,100s,50s,Geki,Katu,Misses,Max Combo,PFC,Beatmap Hash");
-                sw.WriteLine(ry.PlayerName + "," + ry.GameMode + "," + ry.Version + "," + ry.Mods + "," + ry.TotalScore + "," + ry.Count300 + "," + ry.Count100 + "," + ry.Count50 + "," + ry.CountGeki + "," + ry.CountKatu + "," + ry.CountMiss + "," + ry.MaxCombo + "," + ry.IsPerfect + "," + ry.MapHash);
-                sw.Close();
+                sw.WriteLine("Name,Mode,Version,Mods,Score,300s,100s,50s,Geki,Katu,Misses,Max Combo,PFC,Date Played,Beatmap Hash");
+                sw.Close(); //NTODO: Add ability to configure ^
             }
 
-            Console.WriteLine("Finished writing to "+("Replays.csv"));
+            string[] replayList = Directory.GetFiles(replayPath, "*.osr");
+            foreach (string replay in replayList)
+            {
+                Console.WriteLine("Writing " + replay);
+                Replay ry = new Replay(replay, true);
+                if (File.Exists("Replays.csv"))
+                {
+                    using (StreamWriter sw = File.AppendText("Replays.csv"))
+                    {
+                        sw.WriteLine(ry.PlayerName + "," + ry.GameMode + "," + ry.Version + "," + ry.Mods + "," + ry.TotalScore + "," + ry.Count300 + "," + ry.Count100 + "," + ry.Count50 + "," + ry.CountGeki + "," + ry.CountKatu + "," + ry.CountMiss + "," + ry.MaxCombo + "," + ry.IsPerfect + "," + ry.PlayTime + "," + ry.MapHash);
+                        sw.Close(); //NTODO: Along with configurability, clean up this so it's not 2 billion characters long ^
+                    }
+                }
+            }
+
+            Console.WriteLine("Press C to open CSV in default program");
             Console.WriteLine("Press enter to close...");
-            Console.ReadLine();
+            if (Console.ReadKey().Key == ConsoleKey.C)
+                Process.Start("Replays.csv");
         }
     }
 }
