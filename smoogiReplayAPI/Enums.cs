fb0e17eaab942c515d1e9dcc776b41d5609455d0
Nikku <nikkucoding@gmail.com>
2025-09-27 22:13:02 -0400

    Fix newer mods displaying the id and abbreviate them

--- smoogiReplayAPI/Enums.cs
+++ smoogiReplayAPI/Enums.cs
@@ -16,26 +16,37 @@ namespace ReplayAPI
     public enum Mods
     {
         None = 0,
-        NoFail = (1 << 0),
-        Easy = (1 << 1),
-        NoVideo = (1 << 2),
-        Hidden = (1 << 3),
-        HardRock = (1 << 4),
-        SuddenDeath = (1 << 5),
-        DoubleTime = (1 << 6),
-        Relax = (1 << 7),
-        HalfTime = (1 << 8),
-        NightCore = (1 << 9),
-        FlashLight = (1 << 10),
+        NF = (1 << 0), //NoFail
+        EZ = (1 << 1), //Easy
+        TD = (1 << 2), //TouchDevice
+        HD = (1 << 3), //Hidden
+        HR = (1 << 4), //HardRock
+        SD = (1 << 5), //SuddenDeath
+        DT = (1 << 6), //DoubleTime
+        RX = (1 << 7), //Relax
+        HT = (1 << 8), //HalfTime
+        NC = (1 << 9), //NightCore
+        FL = (1 << 10), //FlashLight
         Auto = (1 << 11),
-        SpunOut = (1 << 12),
-        AutoPilot = (1 << 13),
-        Perfect = (1 << 14),
-        Mania4K = (1 << 15),
-        Mania5K = (1 << 16),
-        Mania6K = (1 << 17),
-        Mania7K = (1 << 18),
-        Mania8K = (1 << 19),
+        SO = (1 << 12), //SpunOut
+        AP = (1 << 13), //AutoPilot
+        PF = (1 << 14), //Perfect
+        K4 = (1 << 15), //Key4
+        K5 = (1 << 16), //Key5
+        K6 = (1 << 17), //Key6
+        K7 = (1 << 18), //Key7
+        K8 = (1 << 19), //Key8
+        FI = (1 << 20), //FadeIn
+        RD = (1 << 21), //Random
+        CM = (1 << 22), //Cinema
+        TP = (1 << 23), //TargetPractice
+        K9 =  (1 << 24), //Key9
+        CO = (1 << 25), //Coop
+        K1 = (1 << 26), //Key1
+        K3 = (1 << 27), //Key3
+        K2 = (1 << 28), //Key2
+        V2 = (1 << 29), //ScoreV2
+        MR = (1 << 30) //Mirror
     }
 
     public enum GameModes
