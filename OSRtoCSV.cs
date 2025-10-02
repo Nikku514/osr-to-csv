a39a96552ecbdbf71f7950fb58a603e7b4e65f3b
Nikku <nikkucoding@gmail.com>
2025-10-02 00:55:39 -0400

    Small cleanup

--- OSRtoCSV.cs
+++ OSRtoCSV.cs
@@ -12,6 +12,9 @@ namespace OSRtoCSV
             string replayPath;
             string outputFile;
             
+            //Temp implementation 
+            string csvHeaders = ("Name,Mode,Version,Mods,Score,300s,100s,50s,Geki,Katu,Misses,Max Combo,PFC,Date Played,Beatmap Hash");
+            
             if (File.Exists("Options.cfg"))
             {
                 StreamReader sr = new StreamReader("Options.cfg");
@@ -29,41 +32,33 @@ namespace OSRtoCSV
                 Console.WriteLine("output file is set to " + outputFile);
                 StreamWriter swo = new StreamWriter("Options.cfg");
                 swo.WriteLine(replayPath);
-                swo.WriteLine(outputFile + ".csv");
+                swo.WriteLine($"{outputFile}.csv");
                 swo.Close();
             }
 
             if (!File.Exists(outputFile))
             {
                 //This looks ugly to me but it works
-                if (!(outputFile == ".csv" || outputFile == null || outputFile == ""))
+                if (!(outputFile == ".csv" || string.IsNullOrEmpty(outputFile)))
                 {
                     StreamWriter sw = new StreamWriter(outputFile);
-                    sw.WriteLine("Name,Mode,Version,Mods,Score,300s,100s,50s,Geki,Katu,Misses,Max Combo,PFC,Date Played,Beatmap Hash");
-                    sw.Close(); //NTODO: Add ability to configure ^
+                    sw.WriteLine(csvHeaders);
+                    sw.Close(); //NTODO: Add ability to configure csvHeaders
                 }
                 else
                 {
                     outputFile = "Replays.csv";
                     StreamWriter sw = new StreamWriter(outputFile);
-                    sw.WriteLine("Name,Mode,Version,Mods,Score,300s,100s,50s,Geki,Katu,Misses,Max Combo,PFC,Date Played,Beatmap Hash");
-                    sw.Close(); //NTODO: Add ability to configure ^
+                    sw.WriteLine(csvHeaders);
+                    sw.Close(); //NTODO: Add ability to configure csvHeaders
                 }
             }
 
-            if (replayPath == null || outputFile == "")
+            if (string.IsNullOrEmpty(replayPath) || string.IsNullOrEmpty(outputFile) || outputFile == ".csv")
             {
                 Console.WriteLine("Replay path is empty");
                 File.Move("Options.cfg", "Options." + DateTime.Now.Ticks);
-                Console.WriteLine(outputFile + " renamed to " + outputFile + "." + DateTime.Now.Ticks);
-                return;
-            }
-            // This should be better but ngl cant be assed rn
-            if (outputFile == ".csv" || outputFile == null || outputFile == "")
-            {
-                Console.WriteLine("Output file is empty");
-                File.Move("Options.cfg", "Options." + DateTime.Now.Ticks);
-                Console.WriteLine(outputFile + " renamed to " + outputFile+"." + DateTime.Now.Ticks);
+                Console.WriteLine("Options.cfg renamed to Options." + DateTime.Now.Ticks);
                 return;
             }
             
@@ -72,14 +67,22 @@ namespace OSRtoCSV
             {
                 Console.WriteLine("Writing " + replay);
                 Replay ry = new Replay(replay, true);
+                
+                //Temp implementation
+                string csvReplayData = (ry.PlayerName + "," + ry.GameMode + "," + ry.Version + ",\"" + ry.Mods + "\"," +
+                                        ry.TotalScore + "," + ry.Count300 + "," + ry.Count100 + "," + ry.Count50 + "," +
+                                        ry.CountGeki + "," + ry.CountKatu + "," + ry.CountMiss + "," + ry.MaxCombo + "," +
+                                        ry.IsPerfect + "," + ry.PlayTime + "," + ry.MapHash);
+                
                 if (!File.Exists(outputFile)) continue;
                 using (StreamWriter sw = File.AppendText(outputFile))
                 {
-                    sw.WriteLine(ry.PlayerName + "," + ry.GameMode + "," + ry.Version + ",\"" + ry.Mods + "\"," + ry.TotalScore + "," + ry.Count300 + "," + ry.Count100 + "," + ry.Count50 + "," + ry.CountGeki + "," + ry.CountKatu + "," + ry.CountMiss + "," + ry.MaxCombo + "," + ry.IsPerfect + "," + ry.PlayTime + "," + ry.MapHash);
-                    sw.Close(); //NTODO: Along with configurability, clean up this so it's not 2 billion characters long ^
+                    sw.WriteLine(csvReplayData);
+                    sw.Close(); //NTODO: csvReplayData configurability 
                 }
             }
 
+            Console.WriteLine($"Wrote to {outputFile}");
             Console.WriteLine("Press C to open CSV in default program");
             Console.WriteLine("Press enter to close...");
             if (Console.ReadKey().Key == ConsoleKey.C)
